a6bff7e9e91187ca152fc17b6b51743d20b59746
abolfazl-talebzadeh <abolfazl.talebzadeh@gmail.com>
2022-04-22 13:11:42 +0000

    3:11 p.m 04/22/2022

--- Logs.md
+++ Logs.md
@@ -33,3 +33,42 @@ notes:
     AFFINN word weight dictionary used in python dict
     tokenized comments are stored in python dictionary
     python statistics lib used for calcualting mean
+
+
+--------------------------------------------------------------------------------------------
+
+Hadoop mrJob sentiment Analysis
+cluster:
+    1 namenode + 3 workers
+    node specs:
+        RAM: 4GB
+        VCPUs: 2 VCPU
+        Disk: 40GB
+User time (seconds): 36.74
+System time (seconds): 6.72
+Percent of CPU this job got: 1%
+Elapsed (wall clock) time (h:mm:ss or m:ss): 56:44.34
+Average shared text size (kbytes): 0
+Average unshared data size (kbytes): 0
+Average stack size (kbytes): 0
+Average total size (kbytes): 0
+Maximum resident set size (kbytes): 276020
+Average resident set size (kbytes): 0
+Major (requiring I/O) page faults: 0
+Minor (reclaiming a frame) page faults: 414946
+Voluntary context switches: 150189
+Involuntary context switches: 41827
+Swaps: 0
+File system inputs: 0
+File system outputs: 7144
+Socket messages sent: 0
+Socket messages received: 0
+Signals delivered: 0
+Page size (bytes): 4096
+Exit status: 0
+
+notes:
+    cleaning technic re lib, regEx
+    AFFINN word weight dictionary used in python dict
+    tokenized comments are stored in python dictionary
+    python len() and sum() commands used for calculating average 
---
+++ sentiment-mc.py
@@ -0,0 +1,48 @@
+from mrjob.job import MRJob
+import csv
+import re
+
+class MRCountSum(MRJob):
+    def mapper(self, _, line):
+        r = {}
+        counter = 0
+        score = 0
+        resourceList = []
+        with open("/home/ubuntu/AFINN-111.txt", 'r') as f:
+            data = csv.reader(f,delimiter='\t')
+            resourceList= list(data)
+        for l in resourceList:
+            r[l[0]]=float(l[1])
+        wordDic={}
+        line = line.strip() # remove leading and trailing whitespace
+        lineList = line.split("\t")
+        country = lineList[13]
+        date = lineList[14]
+        date = date[0:7]
+        cleanText = re.sub(r"[0-9(-_:;,.!?@#$%^&*)']",'',country.lower())
+        textList = cleanText.split(" ")
+        t = country.lower()
+        textList = t.split(" ")
+        for i in textList:
+            if i in r.keys():
+                if i in wordDic.keys():
+                    wordDic[i]+=r[i]
+                    counter+=1
+                    score +=r[i]
+                else:
+                    counter+=1
+                    wordDic[i]=r[i]
+                    score+=r[i]
+        if score != 0:
+            out=score/counter
+        else:
+            out = score
+        yield  date, out
+        
+    def reducer(self, key, values):
+        vList = list(values)
+        m = sum(vList)/len(vList)
+        yield key, m
+
+if __name__ == '__main__':
+    MRCountSum.run()
